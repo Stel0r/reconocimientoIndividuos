f5a81b4d464a37b7864819142cda7e57c1ea265d
Diego Gamez <65517739+Stel0r@users.noreply.github.com>
2024-03-19 13:33:46 -0500

    Finalizacion Interfaz e implementacion Reconocimientot

--- deteccion.py
+++ deteccion.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore,QtWidgets,QtGui
 import sys
+import math
 
 ''' 
 Facial Landmark Detection in Python with OpenCV
@@ -97,12 +98,13 @@ class App:
         ventanaLayout = QtWidgets.QGridLayout()
 
 
-        lista = QtWidgets.QListWidget()
-        lista.setStyleSheet("font-size:16px;background-color:#EFEFEF")
-        lista.addItems(archivos)
-        lista.setFixedWidth(200)
-        lista.itemClicked.connect(self.itemChanged)
-        ventanaLayout.addWidget(lista,0,0)
+        self.lista = QtWidgets.QListWidget()
+        self.lista.setStyleSheet("font-size:16px;background-color:#EFEFEF")
+        self.lista.addItems(archivos)
+        self.lista.setFixedWidth(200)
+        self.lista.itemClicked.connect(self.itemChanged)
+        self.lista.setCurrentRow(0)
+        ventanaLayout.addWidget(self.lista,0,0)
 
         panelVista = QtWidgets.QWidget()
         panelVista.setStyleSheet("background-color:#EFEFEF")
@@ -111,7 +113,7 @@ class App:
         ventanaLayout.addWidget(panelVista,0,1)
 
         self.imgLabel = QtWidgets.QLabel()
-        img = QtGui.QPixmap("Harry 1.jpeg").scaled(300,300,aspectRatioMode=QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+        img = QtGui.QPixmap(archivos[0]).scaled(300,300,aspectRatioMode=QtCore.Qt.AspectRatioMode.KeepAspectRatio)
         self.imgLabel.setPixmap(img)
         panelVistaLayout.addWidget(self.imgLabel,0,0,QtCore.Qt.AlignmentFlag.AlignCenter)
         
@@ -138,38 +140,127 @@ class App:
     def clasificar(self):
         text,ok = QtWidgets.QInputDialog().getText(self.ventana,"Clasificacion","como se llama esta persona ?")
         if text and ok:
-            print(text)
+            if text not in sujetos:
+                sujetos[text] = []
+                
+            if len(sujetos[text]) == 2:
+                mensaje = QtWidgets.QMessageBox(self.ventana)
+                mensaje.setText("el sujeto ya tiene dos imagenes guia")
+                mensaje.exec_()
+                return
+            
+            frame = cv2.imread(self.lista.currentItem().text())
+
+            # convert frame to grayscale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Detect faces using the haarcascade classifier on the "grayscale image"
+            faces = detector.detectMultiScale(gray)
+                
+            if(len(faces) == 0):
+                mensaje = QtWidgets.QMessageBox(self.ventana)
+                mensaje.setText("No se encuentran caras en la img")
+                mensaje.exec_()
+                return
+
+            for (x,y,w,d) in faces:
+                # Detect landmarks on "gray"
+                _, landmarks = landmark_detector.fit(gray, np.array(faces))
+
+                for landmark in landmarks:
+                    for x,y in landmark[0]:
+                        # display landmarks on "frame/image,"
+                        # with blue colour in BGR and thickness 2
+                        cv2.circle(frame, (int(x), int(y)), 1, (255, 0, 0), 2)
+                        
+                        
+            # save last instance of detected image
+            cv2.imwrite('result/face-detect.jpg', frame)
+            sujetos[text].append(landmarks)
+            self.lista.takeItem(self.lista.currentRow())
+            self.itemChanged(self.lista.currentItem())
+                
     
     def adivinar(self):
-        print("Es una Persona! creo...")
+        #obtener alpha y beta para la img
+        similitud = {}
+        for sujeto in sujetos:
+            #verifica que el sujeto tiene suficientes imagenes prueba
+            if len(sujetos[sujeto]) != 2:
+                continue
+            suma = 0
+            img1 = sujetos[sujeto][0][0][0]
+            img2 = sujetos[sujeto][1][0][0]
+            
+            xp1i1 = img1[0][0]
+            xp2i1 = img1[1][0] 
+            xp1i2 = img2[0][0]
+            xp2i2 = img2[1][0]
+            
+            #obtiene los puntos de la imagen siendo analizada
+            frame = cv2.imread(self.lista.currentItem().text())
+            # convert frame to grayscale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Detect faces using the haarcascade classifier on the "grayscale image"
+            faces = detector.detectMultiScale(gray)
+                
+            if(len(faces) == 0):
+                mensaje = QtWidgets.QMessageBox(self.ventana)
+                mensaje.setText("No se encuentran caras en la img")
+                mensaje.exec_()
+                return
+                
+            for (x,y,w,d) in faces:
+                # Detect landmarks on "gray"
+                _, landmarks = landmark_detector.fit(gray, np.array(faces))
+
+                for landmark in landmarks:
+                    for x,y in landmark[0]:
+                        # display landmarks on "frame/image,"
+                        # with blue colour in BGR and thickness 2
+                        cv2.circle(frame, (int(x), int(y)), 1, (255, 0, 0), 2)
+            # save last instance of detected image
+            cv2.imwrite('result/face-detect.jpg', frame)
+            img0 = landmarks[0][0]
+            xp1i0 = img0[0][0]
+            xp2i0 = img0[1][0]
+            #obtiene los componentes alpha y beta
+            alpha = ((xp1i0*xp2i2) - (xp2i0*xp1i2))/((xp1i1*xp2i2)-(xp2i1*xp1i2))
+            beta = ((xp2i0*xp1i1)-(xp1i0*xp2i1))/((xp1i1*xp2i2)-(xp2i1*xp1i2))
+            
+            
+            for punto in range(len(img0)):
+                puntoEstimado = alpha*img1[punto][0] + beta*img2[punto][0]
+                parecido = (1 - math.fabs((img0[punto][0]-puntoEstimado)/img0[punto][0]))
+                suma = suma + parecido
+            similitud[sujeto] = suma/len(img0)
+            
+            
+        if len(similitud) == 0:
+            mensaje = QtWidgets.QMessageBox(self.ventana)
+            mensaje.setText("No se tiene suficientes datos para adivinar")
+            mensaje.exec_()
+            return
+        
+        print(similitud)
+        persona = max(similitud,key=similitud.get)
+        if(similitud[persona] < 0.90):
+            mensaje = QtWidgets.QMessageBox(self.ventana)
+            mensaje.setText("No se ha reconocido a nadie")
+            mensaje.exec_()
+            return
+        else:
+            mensaje = QtWidgets.QMessageBox(self.ventana)
+            mensaje.setText("Esta persona es "+persona+" ! con una seguridad del "+"{:5.2f}".format(similitud[persona]*100)+"%")
+            mensaje.exec_()
+            return
+            
     
         
 App()
 
 # read webcam
-frame = cv2.imread("Harry 1.jpeg")
-
-# convert frame to grayscale
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-# Detect faces using the haarcascade classifier on the "grayscale image"
-faces = detector.detectMultiScale(gray)
     
-
-for (x,y,w,d) in faces:
-    # Detect landmarks on "gray"
-    _, landmarks = landmark_detector.fit(gray, np.array(faces))
-
-    for landmark in landmarks:
-        for x,y in landmark[0]:
-            # display landmarks on "frame/image,"
-            # with blue colour in BGR and thickness 2
-            cv2.circle(frame, (int(x), int(y)), 1, (255, 0, 0), 2)
-            
-
-
-# save last instance of detected image
-cv2.imwrite('result/face-detect.jpg', frame)    
     
 # Show image
 cv2.imshow("frame", frame)
